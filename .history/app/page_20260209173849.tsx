0995bab20a016dd0567fdd17ddbe999b28110b36
harshhariyani404 <harsh.hariyani404@gmail.com>
2026-02-09 17:42:05 +0530

    just constant file

--- .history/app/page_20260209173849.tsx
+++ .history/app/page_20260209173849.tsx
@@ -1,13 +1,13 @@
 import Exploarbtn from "./components/Exploarbtn";
 import EventCard from "./components/EventCard";
 import { IEvent } from "./database/event.model";
-import { events } from "./lib/constants";
+// import { events } from "./lib/constants";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const page = async () => {
-  // const res = await fetch(`${BASE_URL}/api/events`)
-  // const events = await res.json();
+  const res = await fetch(`${BASE_URL}/api/events`)
+  const events = await res.json();
   return (
     <section>
       <h1 className="text-3xl font-bold text-center mt-10">Welcome to Dev Event App</h1>
@@ -16,11 +16,11 @@ const page = async () => {
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
         <ul className="events">
-          {/* {events && events.length > 0 && events.map((event: IEvent) => (
+          {events && events.length > 0 && events.map((event: IEvent) => (
             <div key={event.title}>
               <EventCard {...event} />
             </div>
-          ))} */}
+          ))}
 
         </ul>
       </div>
